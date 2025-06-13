75b8d48eec882ec497b628540f8e9790871285e9
Līga <asikeligalillija@gmail.com>
2025-06-13 10:01:48 +0300

    Laravel breeze

--- routes/web.php
+++ routes/web.php
@@ -1,12 +1,22 @@
 <?php
 
-use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ProfileController;
 use App\Http\Controllers\BookController;
+use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
 
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
 Route::get('/books', [BookController::class, 'index'])->name('books.index');
 Route::get('/books/create', [BookController::class, 'create'])->name('books.create');
 Route::post('/books/store', [BookController::class, 'store'])->name('books.store');
@@ -14,3 +24,5 @@ Route::get('/books/{id}', [BookController::class, 'show'])->name('books.show');
 Route::get('/books/{id}/edit', [BookController::class, 'edit'])->name('books.edit');
 Route::put('/books/{id}/update', [BookController::class, 'update'])->name('books.update');
 Route::delete('/books/{id}/destroy', [BookController::class, 'destroy'])->name('books.destroy');
+
+require __DIR__.'/auth.php';
